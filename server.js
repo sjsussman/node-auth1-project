efbaa0b95f1d18b767f882ad9f31c511aa31403e
Steven Sussman <sjsussman@gmail.com>
2020-11-09 16:22:09 -0500

    mvp

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
 const express = require('express');
 
-const userRouter = require('router name location');
+const userRouter = require('./routing/userRouter');
+
+const session = require('express-session')
+const sessionStore = require('connect-session-knex')(session)
 
 const server = express();
 
 server.use(express.json());
-server.use('/', userRouter);
+server.use(session({
+    name: 'testcookie',
+    secret: 'this should come from process.env',
+    cookie: {
+        maxAge: 1000 * 60,
+        secure: false,
+        httpOnly: true
+    },
+    resave: false,
+    saveUninitialized: false,
+    store: new sessionStore ({
+        knex: require('./data/db-config.js'),
+        tablename: 'sessions',
+        sessionidfieldname: 'sid',
+        createTable: true,
+        clearInterval: 1000 * 60 * 60
+    })
+}));
+
+
+
+server.use('/api', userRouter);
 
 module.exports = server;
